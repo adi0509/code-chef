392c79bfc15706e6451550203d97573dc3cf510b
parthyadav3105 <parthyadav3105@gmail.com>
2018-07-27 19:36:23 +0530

    Update coin3.c

--- coin3.c
+++ coin3.c
@@ -7,27 +7,25 @@ unsigned long getCoin(unsigned long x)
 		{
 			return x;
 		}
-	else
-	{
-		if(a[(x/2)]==0)
-			a[(x/2)]=getCoin(x/2);
-		if(a[(x/3)]==0)
-			a[(x/3)]=getCoin(x/3);
-		if(a[(x/4)]==0)
-			a[(x/4)]=getCoin(x/4);
-		return a[(x/2)] + a[(x/3)] + a[(x/4)];
-	}
+	if(x<1000000000)
+		if(a[x]!=0)
+			return a[x];
+	
+		int result=getCoin(x/2)+getCoin(x/3)+getCoin(x/4);
+		if(x<1000000000)
+			if(a[x]==0)
+				a[x]=result;
+		return result;
+	
 }
 int  main()
 {
 	unsigned long x;
-	int i=0;
-	while(i<2)
+	while(true)
 	{
 		scanf("%lu",&x);
 		if(x>0 && x<1000000000)
 		printf("%lu\n",getCoin(x));
-		i++;
 	}
 	return 0;
 }
